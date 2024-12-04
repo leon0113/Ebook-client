170f98d3ba0885f70ae127156301dd1bfcac2ad8
Leon <tahjib.leon399@gmail.com>
2024-12-05 02:35:39 +0600

    vercel error fix

--- src/pages/NewBookCreate.tsx
+++ src/pages/NewBookCreate.tsx
@@ -7,7 +7,7 @@ const NewBookCreate: FC = () => {
 
     const handleSubmit = async (data: FormData) => {
         const res = await client.post('/book/create', data);
-        toast(`From NewBookCreate: ${res.data.message}`);
+        toast(res.data);
     }
 
     return (
