b9b90dfd53062ff1335f96ba10fb5492cb1661c7
Leon <tahjib.leon399@gmail.com>
2024-12-05 02:16:15 +0600

    testing in dep

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const client = axios.create({
     baseURL: "https://ebook-server-livid.vercel.app",
-    withCredentials: true,
 });
 
 client.interceptors.request.use(function (config) {
--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,33 +1,34 @@
 import { FC } from "react";
-import { useDispatch } from "react-redux";
-import { Navigate, useSearchParams } from "react-router-dom";
-import { updateProfile } from "../store/slice/auth.slice";
-import Loading from "../components/common/Loading";
-import { parseError } from "../utils/helper";
+// import { useDispatch } from "react-redux";
+// import { Navigate, useSearchParams } from "react-router-dom";
+// import { updateProfile } from "../store/slice/auth.slice";
+// import Loading from "../components/common/Loading";
+// import { parseError } from "../utils/helper";
 
 const Verify: FC = () => {
 
-    const [searchParams] = useSearchParams();
-    const profileInfoString = searchParams.get('profile');
-    const dispatch = useDispatch();
+    // const [searchParams] = useSearchParams();
+    // const profileInfoString = searchParams.get('profile');
+    // const dispatch = useDispatch();
 
-    if (profileInfoString) {
-        try {
-            const profile = JSON.parse(profileInfoString);
+    // if (profileInfoString) {
+    //     try {
+    //         const profile = JSON.parse(profileInfoString);
 
-            dispatch(updateProfile(profile));
+    //         dispatch(updateProfile(profile));
 
-            if (!profile.signedUp) return <Navigate to='/new-user' />
+    //         if (!profile.signedUp) return <Navigate to='/new-user' />
 
-            return <Navigate to='/' />
-        } catch (error) {
-            parseError(error)
-            return <Navigate to='/not-found' />
-        }
-    }
+    //         return <Navigate to='/' />
+    //     } catch (error) {
+    //         parseError(error)
+    //         return <Navigate to='/not-found' />
+    //     }
+    // }
 
     return (
-        <Loading />
+        // <Loading />
+        <p>Working</p>
     )
 };
 
