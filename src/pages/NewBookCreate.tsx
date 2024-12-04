0294d9675dcca0e2d946f1be3e174e7ec4b1ea57
Leon <tahjib.leon399@gmail.com>
2024-12-05 03:44:43 +0600

    fixing

--- src/pages/NewBookCreate.tsx
+++ src/pages/NewBookCreate.tsx
@@ -1,13 +1,11 @@
 import { FC } from "react";
 import BookForm from "../components/common/BookForm";
 import client from "../api/client";
-import toast from "react-hot-toast";
 
 const NewBookCreate: FC = () => {
 
     const handleSubmit = async (data: FormData) => {
         await client.post('/book/create', data);
-        toast("res.data.message");
     }
 
     return (
